a32af246957c6fac86916fb690766d535334fc09
Roger Michaud <cptjohn47@gmail.com>
2013-10-03 12:55:20 -0500

    Add full path to custom toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,7 +21,7 @@ TARGET_CPU_VARIANT := scorpion
 TARGET_CPU_SMP := true
 
 #Kernel toolchain
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7/bin/arm-eabi-
 
 # inherit from qcom-common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
